d5c168b475212d02d5505b3daae5ca84444f4f14
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2026-05-05 19:04:48 +0200

    Clean up

--- src/modules/gene-data-table/GeneDataDashboard.tsx
+++ src/modules/gene-data-table/GeneDataDashboard.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
 import { GeneDetailView } from '../gene-detail/GeneDetailView';
-import { StatusPanel } from './components/StatusPanel';
 import { ExpressionPanel } from './components/ExpressionPanel';
 import { GeneTable } from './components/GeneTable';
 import { Grid, Group, Paper, Stack } from '@mantine/core';
@@ -12,10 +11,9 @@ import { ASSET_URLS } from '../../utils/url';
 
 export const GeneDataDashboard = () => {
   const geneData = useDomainStore((state) => state.geneData);
-  const isDataLoading = useDomainStore((state) => state.isDataLoading);
   const addedTissues = useExpressionStore((state) => state.addedTissues);
 
-  const { error, totalRows, parse } = useCSVParser(ASSET_URLS.GENES_HUMAN);
+  const { parse } = useCSVParser(ASSET_URLS.GENES_HUMAN);
   
   const [visibleIds, setVisibleIds] = useState<string[]>([]);
 
@@ -30,29 +28,22 @@ export const GeneDataDashboard = () => {
   }, []);
 
   return (
-    <Stack gap="sm" h="100vh" style={{ overflow: 'hidden', backgroundColor: 'var(--mantine-color-gray-0)' }}>
-      <StatusPanel 
-        totalRows={totalRows} 
-        isCSVLoading={isDataLoading} 
-        error={error} 
-        hasData={geneData.length > 0} 
-      />
-
+    <Stack pb="lg" style={{ overflow: 'hidden', backgroundColor: 'var(--mantine-color-gray-0)'}}>
       {geneData.length > 0 && (
-        <Group px="md">
+        <Group>
           <ExpressionPanel isExpLoading={isExpLoading} />
         </Group>
       )}
 
       {geneData.length > 0 && (
-        <Grid style={{ flex: 1, minHeight: 0, overflow: 'hidden' }} px="md" pb="md" m={0} columns={12}>
-          <Grid.Col span={{ base: 12, md: 8 }} style={{ display: 'flex', flexDirection: 'column', padding: 0, paddingRight: '8px' }}>
+        <Grid style={{ flex: 1, minHeight: 0, overflow: 'hidden', padding: 8 }} gutter={8} columns={12}>
+          <Grid.Col span={{ base: 12, md: 8 }} style={{ display: 'flex', flexDirection: 'column' }}>
             <GeneTable 
               isExpLoading={isExpLoading} 
               onVisibleIdsChange={handleVisibleIdsChange} 
             />
           </Grid.Col>
-          <Grid.Col span={{ base: 12, md: 4 }} style={{ display: 'flex', flexDirection: 'column', padding: 0, paddingLeft: '8px' }}>
+          <Grid.Col span={{ base: 12, md: 4 }} style={{ display: 'flex', flexDirection: 'column' }}>
             <GeneDetailView addedTissues={addedTissues} />
           </Grid.Col>
         </Grid>
