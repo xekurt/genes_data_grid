2c6e72c6989c0482012ba368605a66a4f0afa4f8
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2026-05-06 12:08:49 +0200

    linting warning fix

--- src/store/useDomainStore.ts
+++ src/store/useDomainStore.ts
@@ -3,6 +3,7 @@ import type { GeneRecord } from '../types/csv';
 
 interface DomainState {
   geneData: GeneRecord[];
+  geneMap: Map<string, GeneRecord>;
   isDataLoading: boolean;
   setGeneData: (data: GeneRecord[]) => void;
   setIsDataLoading: (isLoading: boolean) => void;
@@ -10,7 +11,11 @@ interface DomainState {
 
 export const useDomainStore = create<DomainState>((set) => ({
   geneData: [],
+  geneMap: new Map(),
   isDataLoading: false,
-  setGeneData: (data) => set({ geneData: data }),
+  setGeneData: (data) => set({ 
+    geneData: data,
+    geneMap: new Map(data.map(g => [g.ensembl, g]))
+  }),
   setIsDataLoading: (isLoading) => set({ isDataLoading: isLoading }),
 }));
