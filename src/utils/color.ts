7f43eceee8b77385fce32dae546f6c28e4675c86
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2026-05-06 11:48:10 +0200

    colors util function

---
+++ src/utils/color.ts
@@ -0,0 +1,9 @@
+export const getCategoryColor = (category: string | undefined): string => {
+  if (!category) return '#ced4da';
+  let hash = 0;
+  for (let i = 0; i < category.length; i++) {
+    hash = category.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const h = Math.abs(hash % 360);
+  return `hsl(${h}, 65%, 45%)`;
+};
